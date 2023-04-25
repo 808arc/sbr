522ccb12eae245bf2e89f8b05cc6b7f3ffe9e080
808arc <arcondatrack@gmail.com>
2023-04-25 17:52:28 -0400

    Worked on fx.py, n eed to add multrithreading

---
+++ fx.py
@@ -0,0 +1,11 @@
+import librosa
+#from IPython.display import Audio
+import scipy.io
+
+voice, sr=librosa. load ('/tracks/*.mp3')
+sr 
+#display(Audio(voice, rate=sr))
+
+#display Audio(voice, rate=20000))
+
+scipy. io.wavfile.write ('sbr_tracks/*.mp3', rate=20000, data=voice)
