7c17704f418f778a7becd373a1d8fffc1891079c
808arc <arcondatrack@gmail.com>
2023-05-04 01:21:40 -0400

    audioread.exceptions.NoBackendError may need ffmpeg codec

--- fx.py
+++ fx.py
@@ -2,16 +2,17 @@ import librosa
 import os
 import scipy.io
 
-# Get a list of all the MP3 files in the tracks directory
-mp3_files = [file for file in os.listdir('tracks') if file.endswith('.mp3')]
+def get_slowed():
+    # Get a list of all the MP3 files in the tracks directory
+    mp3_files = [file for file in os.listdir('tracks') if file.endswith('.mp3')]
 
 
-# Loop over the MP3 files and load each one with librosa.load()
-for mp3_file in mp3_files:
-    file_path = os.path.join('tracks', mp3_file)
-    voice, sr = librosa.load(file_path)
-    print(f'{mp3_file}: {sr} Hz')
-    scipy.io.wavfile.write('sbr_tracks/(SLOWED AND REVERB)' + mp3_file[:-4] + '.mp3', rate=15000, data=voice)
+    # Loop over the MP3 files and load each one with librosa.load()
+    for mp3_file in mp3_files:
+        file_path = os.path.join('tracks', mp3_file)
+        voice, sr = librosa.load(file_path)
+        print(f'{mp3_file}: {sr} Hz')
+        scipy.io.wavfile.write('sbr_tracks/(SLOWED AND REVERB)' + mp3_file[:-4] + '.mp3', rate=15000, data=voice)
 
 #voice, sr=librosa. load ('tracks/_crazy bounce 142 bpm prod shadoww x @808 arc.mp3')
 #sr 
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from chart import get_song_list
 from searcher import get_link_search
 from downloader import get_link_loader
+from fx import get_slowed
 
 from datetime import datetime
 
 
 #today_date = datetime.today().strftime('%Y-%m-%d')
-
+#dates = today_date = datetime.today().strftime('%Y-%m-%d')
 dates = ['2023-03-01', '2023-04-01']
 suffix = "x"
 song_list = get_song_list(suffix, dates)
@@ -17,7 +18,7 @@ list_search = get_link_search(song_list)
 
 link_loader = get_link_loader(list_search)
 
-
+get_slowed()
 
 #print(track_list_search)
 
